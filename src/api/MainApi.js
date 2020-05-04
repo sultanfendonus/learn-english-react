9f2ce8956b38b1f1342f768c5c1b0c6a79fafe81
sultan <sultan@priyo.com>
2020-05-05 01:13:50 +0600

    Deploy

--- src/api/MainApi.js
+++ src/api/MainApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// export const baseUrl = 'https://english-api.factandfun.com'
-export const baseUrl = 'http://127.0.0.1:3002'
+export const baseUrl = 'https://english-api.factandfun.com'
+//export const baseUrl = 'http://127.0.0.1:3002'
 export default axios.create({
     baseURL : baseUrl,
     headers: {
