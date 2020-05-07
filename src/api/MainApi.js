1ecfc24c4d8384c701a622239ceabee27b9cbd29
sultan <sultan@priyo.com>
2020-05-08 00:33:51 +0600

    [49]:Handled Unauthorized users.

--- src/api/MainApi.js
+++ src/api/MainApi.js
@@ -1,9 +1,25 @@
 import axios from 'axios';
-export const baseUrl = 'https://english-api.factandfun.com'
-//export const baseUrl = 'http://127.0.0.1:3002'
-export default axios.create({
+
+//export const baseUrl = 'https://english-api.factandfun.com'
+export const baseUrl = 'http://127.0.0.1:3002'
+const MainApi = axios.create({
     baseURL : baseUrl,
     headers: {
         "Authorization": `Token ${localStorage.getItem("token")}`
     }
 })
+
+const UNAUTHORIZED = 401;
+MainApi.interceptors.response.use(
+    response => response,
+    error => {
+        const {status} = error.response;
+        if (status === UNAUTHORIZED) {
+            window.localStorage.removeItem('token')
+            window.location.href = "/"
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default MainApi
