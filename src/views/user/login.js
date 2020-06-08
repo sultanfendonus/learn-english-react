06df584da07acf64d91338d47084162f55572480
Sultan <sultan.home1640@gmail.com>
2020-06-08 18:00:17 +0600

    [138]: facebook login done.

--- src/views/user/login.js
+++ src/views/user/login.js
@@ -10,6 +10,9 @@ import { loginUser,resetError } from "../../actions";
 import { Colxx } from "../../components/common/CustomBootstrap";
 import IntlMessages from "../../helpers/IntlMessages";
 import FacebookLoginContainer from "../../components/facebook-login/FacebookLoginContainer";
+import {Button as MButton} from '@material-ui/core';
+import FacebookIcon from '@material-ui/icons/Facebook';
+
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -144,7 +147,16 @@ class Login extends Component {
               </Formik>
               <p>Don't have an account? <a style={{color: 'blue'}} href="/user/register">Register Now!</a></p>
 
-              {/*<FacebookLoginContainer/>*/}
+              <MButton
+                  variant="contained"
+                  // color="#4556ac"
+                  style={{backgroundColor: '#4556ac', color: 'white', marginTop: 10}}
+                  startIcon={<FacebookIcon />}
+                  onClick={()=>window.location.href = `${process.env.REACT_APP_BASE_URL}/login/facebook`}
+              >
+                Login with facebook
+              </MButton>
+
 
             </div>
           </Card>
