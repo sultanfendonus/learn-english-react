5e0da871e46eb525f602fa7d6093b2062cb9caab
Sultan <sultan.home1640@gmail.com>
2020-06-12 01:43:48 +0600

    [136]: Increased font size to 18 in all input text field.

--- src/components/feedback/NewFeedbackContainer.js
+++ src/components/feedback/NewFeedbackContainer.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {newFeedback} from "../../actions/index"
-import { Card } from 'antd';
-import { Input, message } from 'antd';
-import { Select, Button } from 'antd';
+import {Card} from 'antd';
+import {Input, message} from 'antd';
+import {Select, Button} from 'antd';
 
-const { Option } = Select;
-const { TextArea } = Input;
+const {Option} = Select;
+const {TextArea} = Input;
 let self;
 
 function mapStateToProps(state) {
@@ -14,7 +14,7 @@ function mapStateToProps(state) {
 }
 
 class NewFeedbackContainer extends Component {
-    state={category: null, name: null, message: null}
+    state = {category: null, name: null, message: null}
 
     componentDidMount() {
         self = this;
@@ -25,26 +25,32 @@ class NewFeedbackContainer extends Component {
         self.setState({category: value})
     }
 
-    submitForm(){
-        if(this.state.message){
-            this.props.newFeedback({user_name: this.state.name, category: this.state.category, message: this.state.message})
+    submitForm() {
+        if (this.state.message) {
+            this.props.newFeedback({
+                user_name: this.state.name,
+                category: this.state.category,
+                message: this.state.message
+            })
             this.setState({category: null, name: null, message: null})
-        }else {
-         message.error("Please write some feedback.")
+        } else {
+            message.error("Please write some feedback.")
         }
     }
 
     render() {
         return (
             <div>
-                <Card title="New Feedback" style={{ }}>
+                <Card title="New Feedback" style={{}}>
                     <p style={{fontWeight: 'bolder'}}>Name:</p>
-                    <Input value={this.state.name} onChange={(e)=>this.setState({name: e.target.value})} style={{marginBottom: '10px'}} placeholder="Name" />
+                    <Input
+                        value={this.state.name} onChange={(e) => this.setState({name: e.target.value})}
+                        style={{marginBottom: '10px', fontSize: 18}} placeholder="Name"/>
 
                     <p style={{fontWeight: 'bolder'}}>Category:</p>
                     <Select
                         showSearch
-                        style={{ width: 300, marginBottom: '10px' }}
+                        style={{width: 300, marginBottom: '10px'}}
                         placeholder="Select a feedback category"
                         optionFilterProp="children"
                         onChange={this.onChange}
@@ -58,9 +64,10 @@ class NewFeedbackContainer extends Component {
                     </Select>
 
                     <p style={{fontWeight: 'bolder'}}>Your feedback/message:</p>
-                    <TextArea value={this.state.message} style={{marginBottom: '15px'}} onChange={(e)=>this.setState({message: e.target.value})} rows={8} />
+                    <TextArea value={this.state.message} style={{marginBottom: '15px'}}
+                              onChange={(e) => this.setState({message: e.target.value})} rows={8}/>
 
-                    <Button onClick={()=>this.submitForm()} type="primary">Submit</Button>
+                    <Button onClick={() => this.submitForm()} type="primary">Submit</Button>
 
                 </Card>
 
